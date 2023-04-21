57986395bce18105f8175d8f63c126ca1e140769
Xcaliburns <davidabruzzogames@gmail.com>
2023-04-21 10:19:41 +0200

    polished

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
-
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-
+import Link from "next/link";
+import Date from "../components/date";
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -17,34 +17,23 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>        
+        <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Ceci est un site de test mais je dois dire que next est génial</p>
+
        
-        <div>  <p className="styled">un paragraphe avec styled JSX, c'est pas beau mais c'est là</p>
-            <style jsx>{`
-        .styled {
-          margin: 50px;
-        }
-        p {
-          color: blue;
-          background-color:red;
-          text-align:center;
-        }
-      `}</style></div>
-         
       </section>
-       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
